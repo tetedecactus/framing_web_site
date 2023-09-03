d4cdafb0a1210692e35bb9199fd1ccf8ac1033b8
Pro-Lames <o.labrecque.lacasse@gmail.com>
2023-09-03 19:56:33 -0400

    ++

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,38 @@
 <template>
     <div class="about-page-container">
-        <h2>East Claw Contruction Inc</h2>
-        <h3>About Us</h3>
-        <p>East Claw Construction inc. is Edmonton's premier choice for top-notch framing solutions. With a reputation built on excellence and reliability, we specialize in both home framing and commercial framing projects. Our team of skilled professionals is dedicated to delivering superior craftsmanship, ensuring the structural integrity and longevity of every building we touch.
-            At East Claw Construction, we understand the importance of a strong foundation. We take pride in our attention to detail, precision engineering, and adherence to the highest industry standards. Whether it's a residential property or a large-scale commercial development, our commitment to excellence remains unwavering.
-            When you choose East Claw Construction, you're choosing a partner that values quality, timeliness, and client satisfaction. Trust us to bring your framing projects to life with expertise and efficiency, turning your vision into reality. Contact us today to discuss your framing needs, and let's build a future together.</p>
+        <div class="bg-pageAboutImg">
+            <div>
+                <h2>East Claw Contruction Inc</h2>
+                <h3>About Us</h3>
+                <p>East Claw Construction inc. is Edmonton's premier choice for top-notch 
+                    framing solutions. With a reputation built on excellence and reliability, 
+                    we specialize in both home framing and commercial framing projects. 
+                    Our team of skilled professionals is dedicated to delivering 
+                    superior craftsmanship, ensuring the structural integrity and 
+                    longevity of every building we touch.<br>
+                    <br>At East Claw Construction, we understand the importance of a 
+                    strong foundation. We take pride in our attention to detail, 
+                    precision engineering, and adherence to the highest industry standards.
+                    Whether it's a residential property or a large-scale commercial 
+                    development, our commitment to excellence remains unwavering.<br><br>
+                    When you choose East Claw Construction, you're choosing a partner 
+                    that values quality, timeliness, and client satisfaction. Trust us to 
+                    bring your framing projects to life with expertise and efficiency, 
+                    turning your vision into reality. Contact us today to discuss your 
+                    framing needs, and let's build a future together.
+                </p>
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+</script>
+
+<style>
+ 
+.bg-pageAboutImg::before {
+  content: "";
+  background-image: url("../assets/images/framing_guy.jpg");
+}
+</style>
