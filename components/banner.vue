a93cd9961a75bbb95686a837d886859afe2efde0
TeteDeCactus <o.labrecque.lacasse@gmail.com>
2023-12-04 22:38:30 -0500

    ++

--- components/banner.vue
+++ components/banner.vue
@@ -9,13 +9,16 @@
         </div>
         <p>any projects <span>large or small</span></p>
         <p>edmonton and <span>around</span></p>
-      </div>
-      <div class="banner-btn text-lg">
+        <button class="banner-btn text-lg mt-16">
+
           <a :href="telLink">
             <img src="../assets/images/icons8-phone-50.png" alt=""/>
           </a>
           <a :href="telLink">Call us at 1-780-271-6505</a>
+        </button>
       </div>
+      <!-- <div class="banner-btn text-lg"> -->
+      <!-- </div> -->
     </div>
   </div>
 </template>
@@ -38,7 +41,10 @@ export default {
 }
 .bg-banner::before {
   content: "";
-  background-image: url("../assets/images/homeImg.jpg");
+  background-image: url("../assets/images/logo_official.svg"), url("../assets/images/homeImg.jpg") ;
+  background-position: center, center; /* Positionnement au centre pour les deux images */
+  background-size: contain, cover; /* Ajustement de la taille pour les deux images */
+  background-repeat: no-repeat;
 }
 
 </style>
